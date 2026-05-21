d5d0f49f8e20872bb62dd8a38acb104e66f767ec
mhidt <mhidt_unsainted@mail.ru>
2026-05-21 12:44:28 +0800

    ✨ Открытие настроек, как отдельной программы

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -4,6 +4,7 @@ pub const SETTINGS_WINDOW_LABEL: &str = "settings";
 
 pub fn show_settings<R: Runtime>(app: &AppHandle<R>) -> tauri::Result<()> {
     if let Some(window) = app.get_webview_window(SETTINGS_WINDOW_LABEL) {
+        window.set_skip_taskbar(false)?;
         window.show()?;
         window.unminimize()?;
         window.set_focus()?;
